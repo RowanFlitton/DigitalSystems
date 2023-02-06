50ece233ab1ef3dbef576d65ebf2a567eab77b5d
RowanFlitton <91536679+RowanFlitton@users.noreply.github.com>
2023-02-06 10:52:15 +0000

    tasks completed

--- tasks/Task215-2-to-1-multiplexer/mux2_behavioural_v2.sv
+++ tasks/Task215-2-to-1-multiplexer/mux2_behavioural_v2.sv
@@ -1,10 +1,10 @@
 module mux2_behavioural_v2 (output logic y, input logic a, b, s);
-always_comb
-	if (s) begin
-	   y = b;	//Single line does not need begin
-	end
-	else begin
-	   y = a;
-        end
+wire m1,m2,not_s;
+
+//Instantiate and connect logic gates
+not u1(not_s, s);
+and u2(m1, not_s, a);
+and u3(m2, s, b);
+or  u4(y, m1, m2);
 
 endmodule
